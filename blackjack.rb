3a6729ca9ffc14f24b538da9d86247e800b4364f
John Morgan <jtmorgan61@gmail.com>
2014-01-07 21:14:09 -0800

    revisions from code reviews, thanks guys

--- blackjack.rb
+++ blackjack.rb
@@ -1,106 +1,66 @@
-# This blackjack program has been coded without looking at any solutions.
-
-def make_deck(vals, suits, deck_num)
-  deck = []
-  deck_num.times do
-    vals.each do |val|
-      suits.each do |suit|
-        deck << [val, suit]
-      end
-    end
+def single_deal(deck, cards, player = 1)
+  card = deck.pop  
+  if player == 1
+    puts
+    puts "You draw a #{card[0]} of #{card[1]}"
+  else 
+    puts
+    puts "The dealer draws a #{card[0]} of #{card[1]}"
   end
-  deck
-end
-
-def shuffle(deck)
-  return recursive_shuffle(deck, [])
-end
-
-def recursive_shuffle(deck, shuffled_deck)
-  if deck.length == 0
-    return shuffled_deck
-  else
-    shuffled_deck.push(deck.slice!(rand(deck.length)))
-    return recursive_shuffle(deck, shuffled_deck) 
-  end
-end
-
-def deal_card(deck)
-  card = deck.pop
-end
-
-def start_deal(deck)
-  cards = []
-  2.times do
-    cards << deal_card(deck)
-  end
-  cards
-end
-
-def single_deal(deck, cards)
-  card = deal_card(deck)  
-  puts "You draw a #{card[0]} of #{card[1]}"
-  cards << card
-end
-
-def dealer_deal(deck, cards)
-  card = deal_card(deck)
-  puts "The dealer draws a #{card[0]} of #{card[1]}"
   cards << card
 end
 
 def score(cards)
-  score = 0
-
-  cards.each do |card|
-    case card[0]
-    when '2' then score += 2
-    when '3' then score += 3
-    when '4' then score += 4
-    when '5' then score += 5
-    when '6' then score += 6
-    when '7' then score += 7
-    when '8' then score += 8
-    when '9' then score += 9
-    when '10' then score += 10
-    when 'jack' then score += 10
-    when 'queen' then score += 10
-    when 'king' then score += 10
+  total = 0
+  vals = cards.map{ |e| e[0] }
+  vals.each do |val|
+    if val == "Ace"
+      total += 11
+    elsif val.to_i == 0 
+      total += 10
+    else
+      total += val.to_i
     end
   end
 
-  num_aces = 0
-  cards.each do |card|
-    if card[0] == "Ace"
-      num_aces += 1
-    end
+  vals.select{ |e| e == "Ace" }.count.times do
+      total -= 10 if total > MAX_SCORE
   end
 
-  if num_aces > (11 - score)
-    cards.each do |card|
-      if card[0] == "Ace"
-        score += 1
-      end
+  total
+end
+
+def blackjack_check(player_cards, player_score, dealer_cards, dealer_score, name)
+  blackjack = false
+  if player_score == MAX_SCORE
+    blackjack = true
+    read_cards(player_cards, player_score, dealer_cards, name)
+    puts "Blackjack!"
+    if dealer_score != MAX_SCORE
+      puts "You win! Congrats!"
+    else 
+      read_dealer_cards(dealer_cards, dealer_score)
+      puts "But the dealer also has blackjack. You draw."
     end
-  else
-    cards.each do |card|
-      if card[0] == "Ace"
-        score > 10 ? score += 1 : score += 11
-      end
-    end
+  elsif dealer_score == MAX_SCORE
+    blackjack = true
+    read_cards(player_cards, player_score, dealer_cards, name)
+    read_dealer_cards(dealer_cards, dealer_score)
+    puts "Sorry, the dealer has blackjack. You lose."
   end
-  score
+  blackjack
 end
 
 def play_player_hand(deck, player_cards, player_score, dealer_cards, name)
   read_cards(player_cards, player_score, dealer_cards, name)
   while true  
+    puts
     puts "What would you like to do, #{name}? Type 'stay' or 'hit':"
     input = gets.chomp
     if input == "stay"
       break
     elsif input == "hit"
-      player_cards = single_deal(deck, player_cards)
+      player_cards = single_deal(deck, player_cards, player = 1)
       player_score = score(player_cards)
       read_cards(player_cards, player_score, dealer_cards, name)
     else
@@ -109,7 +69,7 @@ def play_player_hand(deck, player_cards, player_score, dealer_cards, name)
       read_cards(player_cards, player_score, dealer_cards, name)
     end
 
-    if player_score > 21
+    if player_score > MAX_SCORE
       puts "You busted! Sorry, #{name}, but you lose."
       break
     end
@@ -118,34 +78,37 @@ def play_player_hand(deck, player_cards, player_score, dealer_cards, name)
 end
 
 def read_cards(cards, score, deal_cards, name)
+  puts
   puts "You have:"
   cards.each do |card|
     puts "#{card[0]} of #{card[1]}"
   end
+  puts
   puts "Your score: #{score}"
   puts "Dealer showing: #{deal_cards[0][0]} of #{deal_cards[0][1]}"
 end
 
-def deal_read_cards(cards, score)
+def read_dealer_cards(cards, score)
+  puts
   puts "The dealer has:"
   cards.each do |card|
     puts "#{card[0]} of #{card[1]}"
   end
-  puts "Dealer score: #{score}"
+  puts "Dealer's score: #{score}"
 end
 
 def play_dealer_hand(deck, dealer_cards, dealer_score)
-  while dealer_score < 17
-    dealer_cards = dealer_deal(deck, dealer_cards)
+  while dealer_score < DEALER_CUTOFF
+    dealer_cards = single_deal(deck, dealer_cards, player = 0)
     dealer_score = score(dealer_cards)
-    deal_read_cards(dealer_cards, dealer_score)
+    read_dealer_cards(dealer_cards, dealer_score)
     sleep(1)
   end
   dealer_score
 end
 
 def resolve_game(player_score, dealer_score, name)
-  if dealer_score > 21
+  if dealer_score > MAX_SCORE
     puts "The dealer busts! You win, #{name}."
   else
     puts "#{name}'s score: #{player_score}"
@@ -162,6 +125,7 @@ end
 def play_again?
   play_again = false
   while true
+    puts
     puts "Play again? Please enter 'yes' or 'no'."
     input = gets.chomp
     if input == "yes"
@@ -176,53 +140,62 @@ def play_again?
   play_again
 end
 
-# Adjust number of decks used to prevent counting (bonus question)
-deck_num = 2
-vals = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace']
-suits = %w(clubs diamonds hearts spades)
+
+MAX_SCORE = 21
+DEALER_CUTOFF = 17
 
 puts "Welcome to John Morgan's procedural blackjack game! Please enter name:"
 name = gets.chomp
-deck = shuffle(make_deck(vals, suits, deck_num))
+puts
+puts "Welcome, #{name}!"
+
+# Adjust number of decks used to prevent counting (bonus question).
+deck_num = 5
+vals = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 
+        'Ace']
+suits = %w(Clubs Diamonds Hearts Spades)
+deck = vals.product(suits)
+(deck_num - 1).times do
+  deck = deck + vals.product(suits)
+end
+deck.shuffle!
 
 while true
+  puts
+  puts "The dealer deals the cards..."
   while true
-    player_cards = start_deal(deck)
+    player_cards = [deck.pop]
+    dealer_cards = [deck.pop]
+    player_cards << deck.pop
+    dealer_cards << deck.pop
     player_score = score(player_cards)
-    dealer_cards = start_deal(deck)
     dealer_score = score(dealer_cards)
 
-    if player_score == 21
-      read_cards(player_cards, player_score, dealer_cards, name)
-      puts "Blackjack!"
-      if dealer_score != 21
-        puts "You win! Congrats!"
-      else 
-        puts "But the dealer also has blackjack. You draw."
-      end
-      break
-    end
-    if dealer_score == 21
-      read_cards(player_cards, player_score, dealer_cards, name)
-      puts "Sorry, the dealer has blackjack. You lose."
+    blackjack = blackjack_check(player_cards, player_score, dealer_cards, 
+                                 dealer_score, name)
+    if blackjack
       break
     end
 
-    player_score = play_player_hand(deck, player_cards, player_score, 
+    play_player_hand(deck, player_cards, player_score, 
                                     dealer_cards, name)
+    player_score = score(player_cards)
 
-    if player_score <= 21
-      deal_read_cards(dealer_cards, dealer_score)
+    if player_score <= MAX_SCORE
+      read_dealer_cards(dealer_cards, dealer_score)
       sleep(1)
-      dealer_score = play_dealer_hand(deck, dealer_cards, dealer_score)
-
+      play_dealer_hand(deck, dealer_cards, dealer_score)
+      dealer_score = score(dealer_cards)
+      puts
       resolve_game(player_score, dealer_score, name)
     end
     break
   end
 
   if not play_again?
+    puts
     puts "Thanks for playing!"
+    puts
     break
   end
 end
